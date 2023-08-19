fda5d0a47512bf90bf346605deffbb388d2b4005
Nirav Khant <29600493+niravkhant@users.noreply.github.com>
2023-08-19 19:59:18 +0530

    template react+vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.scss";
+
+function App() {
+    return (
+        <>
+            <h1 className="text-4xl font-bold underline ">Hello world!</h1>
+        </>
+    );
+}
+
+export default App;
